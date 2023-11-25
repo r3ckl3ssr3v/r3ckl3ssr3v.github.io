cb43d4141ab0d279e3f649263a17de90b7a6e600
Sabya Sachi Pal <98833669+r3ckl3ssr3v@users.noreply.github.com>
2023-11-26 01:03:42 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -30,7 +30,7 @@ noBtn.addEventListener("mouseover", () => {
       noBtn.style.left = randomX + "px";
       noBtn.style.top = randomY + "px";
       isDelayActive = false; // Reset the delay flag after shifting the button
-    }, 1000); // 2000 milliseconds = 2 seconds
+    }, 500); // 2000 milliseconds = 2 seconds
   }
 });
 
